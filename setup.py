49ac8af08e83a37a07b14ad327dfa0299324232d
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2021-11-12 11:58:16 +0200

    add mfy_nlp_core project

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+REQUIREMENTS_FILE_NAME = 'requirements.txt'
+
+install_requirements = []
+
+with open(REQUIREMENTS_FILE_NAME) as file:
+    install_requirements = list(map(str.strip,file.read().splitlines()))
+
+setup(
+    name='mfy_nlp_core',
+    packages=find_packages(include=['mfy_nlp_core']),
+    version='0.0.1',
+    description='A library used by the Meaningfy team to manage dependencies to third party libraries for NLP tasks.',
+    author='Meaningfy',
+    license='Apache License 2.0',
+    install_requires=install_requirements,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
