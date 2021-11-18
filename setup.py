6f90731451257f70cede36d91c7cb354f43966ee
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2021-11-18 21:34:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(REQUIREMENTS_FILE_NAME) as file:
 
 setup(
     name='mfy_nlp_core',
-    packages=find_packages(include=['mfy_nlp_core','mfy_nlp_core_fakes']),
+    packages=find_packages(exclude=['test']),
     version='0.0.1',
     description='A library used by the Meaningfy team to manage dependencies to third party libraries for NLP tasks.',
     author='Meaningfy',
